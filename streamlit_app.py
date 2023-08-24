85b116f3eb4a8b09a4912a968d85cd5d8bb8600b
Abdulelah-Aldhafer1 <74058839+Abdulelah-Aldhafer1@users.noreply.github.com>
2023-08-24 10:43:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ import pandas as pd
 # streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
 # streamlit.text('🍞Hard-Boiled Free-Range Egg')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
