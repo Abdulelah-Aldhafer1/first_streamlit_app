766e82eb98c73942949ec2d9dc109dace0609361
Abdulelah-Aldhafer1 <74058839+Abdulelah-Aldhafer1@users.noreply.github.com>
2023-08-30 08:30:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ streamlit.header('🥣Breakfast Menu')
 streamlit.title('build your won smmoithe')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("pick some fruits:",list(my_fruit_list,index))
 streamlit.dataframe(my_fruit_list)
-print("hi")
