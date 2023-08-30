1228952c7f466323e33b8c86a4d229439e6ce059
Abdulelah-Aldhafer1 <74058839+Abdulelah-Aldhafer1@users.noreply.github.com>
2023-08-30 08:27:58 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,4 @@ streamlit.title('build your won smmoithe')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+print("hi")
