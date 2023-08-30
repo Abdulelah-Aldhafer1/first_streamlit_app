90bdd61c8eb3795aa15c7d85a40e28fe87d51878
Abdulelah-Aldhafer1 <74058839+Abdulelah-Aldhafer1@users.noreply.github.com>
2023-08-30 08:31:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,6 @@ streamlit.header('🥣Breakfast Menu')
 streamlit.title('build your won smmoithe')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.multiselect("pick some fruits:",list(my_fruit_list,index))
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
